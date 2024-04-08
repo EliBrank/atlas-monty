9684210cf8ac6e5ed426054dcf472266a280be29
EliBrank <eliasbrinkman@gmail.com>
2024-04-08 12:24:07 -0500

    add additional comments

--- get_instruction.c
+++ get_instruction.c
@@ -26,21 +26,19 @@ int get_instruction(char *op_code, stack_t **head)
 
 	if (tmp != NULL)
 		*push_num = atoi(tmp);
+	/* checks for push without number */
 	else if (strcmp(op_exec, "push") == 0)
     {
 		fprintf(stderr, "L%zu: usage: push integer\n", line_ct);
         return (0);
     }
-	/* if tmp returns NULL, then tokenization produced only one arg */
-	/* if (tmp != NULL)
-		free(tmp); */
 
 	while (ops[i].opcode != NULL)
 	{
 		/* checks opcode, runs matching function */
 		if (strcmp(op_exec, ops[i].opcode) == 0)
 		{
-			free(op_exec);
+			// free(op_exec);
 			return (ops[i].f(head, push_num));
 		}
 		i++;
